28668658040a042f851cdc197f424956100fc231
christylcao <135674330+christylcao@users.noreply.github.com>
2023-06-20 12:30:26 -0500

    anotherone

---
+++ src/test/java/variousConcept/AnotherOne.java
@@ -0,0 +1,18 @@
+package variousConcept;
+
+public class AnotherOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ANOTHER ONE");
+		System.out.println("ANOTHER ONE");
+		
+		System.out.println("ANOTHER ONE");
+		System.out.println("ANOTHER ONE");
+		System.out.println("ANOTHER ONE");
+		System.out.println("ANOTHER ONE");
+		System.out.println("ANOTHER ONE");
+		
+		System.out.println("ANOTHER ONE");
+	}
+}
